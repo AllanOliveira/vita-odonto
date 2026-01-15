f0bf73fa77f2d662c97372a695e829d6ff9acbfa
Allan Costa <allan.oli.90@gmail.com>
2026-01-15 00:28:12 -0300

    atualizando texto do botao de agendamento do header

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -17,7 +17,7 @@
         target="_blank"
         class="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-sm hover:shadow-md transition"
       >
-        WhatsApp
+        Agendar
       </a>
     </nav>
 
